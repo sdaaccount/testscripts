93a50855e33db2d8242da273c3fa8f9c5baef5e5
Damian Wojslaw <damian@wojslaw.pl>
2021-08-07 11:44:01 +0200

    Ask player if they want to play

--- adv.sh
+++ adv.sh
@@ -5,3 +5,15 @@ echo "What is your name, adventurer?"
 read adventurer
 
 echo "Nice to meet you, $adventurer"
+
+#### 
+
+echo "Would you like to play a game? [y/n]"
+
+read decision
+if [[ "$decision" == "y" ]]
+then
+	echo "Great! Lets go!"
+else
+	echo "Sad to see you go, $adventurer!"
+fi
